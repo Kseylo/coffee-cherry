0d8bf6f591f8dddfab5553c500422e22c706f32b
Kseylo <alekseysarychev29@gmail.com>
2022-05-17 09:37:50 +0700

    heroku config

--- config/settings.py
+++ config/settings.py
@@ -80,14 +80,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'coffee-cherry-django',
-        'USER': 'kseylo',
-        'PASSWORD': env.str('PASSWORD'),
-        'HOST': '127.0.0.1',
-        'PORT': '5432'
-    }
+"default": env.dj_db_url("DATABASE_URL")
 }
 
 
