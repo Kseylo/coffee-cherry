839ea7dfdcebced3daff5f0678a3c008e2d8cfa5
Kseylo <alekseysarychev29@gmail.com>
2022-05-17 10:27:24 +0700

    fixes for menu section

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -18,14 +18,16 @@ headerObserver.observe(section);
 const allSections = document.querySelectorAll('.section');
 
 const revealSection = function (entries, observer) {
+  console.log(entries)
   const [entry] = entries;
   if (!entry.isIntersecting) return;
   entry.target.classList.remove('section--hidden');
   observer.unobserve(entry.target);
 };
+
 const sectionObserver = new IntersectionObserver(revealSection, {
   root: null,
-  threshold: 0.15,
+  threshold: 0.10,
 });
 allSections.forEach(function (section) {
   sectionObserver.observe(section);
